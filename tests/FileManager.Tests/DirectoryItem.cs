bdca0bb357eb986601282660676f2143c16ea0c7
george <gchatzopoulos@york.citycollege.eu>
2025-07-15 23:23:55 +0300

    test(DirectoryItem): Add facts for SetName with invalid names and GetFile

--- tests/FileManager.Tests/DirectoryItem.cs
+++ tests/FileManager.Tests/DirectoryItem.cs
@@ -2,6 +2,8 @@ namespace FileManager.Tests;
 
 using FileManager.Models;
 
+using Newtonsoft.Json.Bson;
+
 using System.IO;
 using System.Text;
 
@@ -31,5 +33,26 @@ public class DirectoryItemTests : IDisposable
     {
         var test = new DirectoryItem("test");
         Assert.Equal("test", test.Name);
+    }
+
+    [Fact]
+    public void Constructor_SetInvalidName_ThrowsArgumentException()
+    {
+        Assert.Throws<ArgumentException>(() => new FileItem("01test"));
+        Assert.Throws<ArgumentException>(() => new FileItem("$test"));
+        Assert.Throws<ArgumentException>(() => new FileItem("_test"));
+    }
+
+    [Fact]
+    public void GetFile_FileDoesNotExist_ReturnsNull()
+    {
+        var test = new DirectoryItem("test");
+        Assert.Null(test.GetFile("test_file.txt"));
+    }
+
+    [Fact]
+    public void GetFIle_FileExists_ReturnsFile()
+    {
+        Assert.NotNull(_testFiles.GetFile("a.txt"));
     }
 }

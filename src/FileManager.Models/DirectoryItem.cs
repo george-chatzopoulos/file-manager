4b7cc913a83de6c748a7e86ed1b8b51b4eaaf7ba
george <gchatzopoulos@york.citycollege.eu>
2025-07-27 05:31:28 +0300

    fix(DirectoryItem): Remove files attribute and all files-related methods

--- src/FileManager.Models/DirectoryItem.cs
+++ src/FileManager.Models/DirectoryItem.cs
@@ -11,12 +11,10 @@ public sealed class DirectoryItem
     public int Id { get; set; }
     public string Name { get; private set; } = string.Empty;
     public string Path { get; private set; } = "/";
-    private Dictionary<string, FileItem> _files = [];
 
-    public DirectoryItem(string name, Dictionary<string, FileItem>? files = null)
+    public DirectoryItem(string name)
     {
         SetName(name);
-        _files = new Dictionary<string, FileItem>(files ?? []);
     }
 
     public void SetName(string name)
@@ -25,36 +23,5 @@ public sealed class DirectoryItem
             throw new ArgumentException("Invalid directory name", nameof(name));
 
         Name = name;
-    }
-
-    public FileItem? GetFile(string name)
-    {
-        return _files.TryGetValue(name, out var file) ? file : null;
-    }
-
-    public void AddOrUpdateFile(FileItem file)
-    {
-        _files[file.Name] = file;
-    }
-
-    public bool RemoveFile(string name)
-    {
-        return _files?.Remove(name) ?? false;
-    }
-
-    public bool ContainsFile(string name)
-    {
-        return _files?.ContainsKey(name) ?? false;
-    }
-
-    public IEnumerable<FileItem> GetAllFiles()
-    {
-        return _files?.Values ?? Enumerable.Empty<FileItem>();
-    }
-
-
-    public void Clear()
-    {
-        _files.Clear();
     }
 }

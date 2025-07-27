a5a60ac21bb00d3f16f3f2ac129e861124af03f7
george <gchatzopoulos@york.citycollege.eu>
2025-07-27 06:16:31 +0300

    test(DirectoryItem): Remove all files-related test cases and expanded the Constructor_SetInvalidName_ThrowsArgumentException fact

--- tests/FileManager.Models.Tests/DirectoryItem.cs
+++ tests/FileManager.Models.Tests/DirectoryItem.cs
@@ -1,31 +1,11 @@
 namespace FileManager.Models.Tests;
 
-using System.IO;
-using System.Text;
-
 using FileManager.Models;
 
 using Xunit;
 
-public class DirectoryItemTests : IDisposable
+public class DirectoryItemTests
 {
-    private readonly DirectoryItem _testFiles = new("test");
-    private readonly FileItem _a = new("a.txt");
-    private readonly FileItem _b = new("b.txt");
-    private readonly FileItem _c = new("c.txt");
-
-    public DirectoryItemTests()
-    {
-        _testFiles.AddOrUpdateFile(_a);
-        _testFiles.AddOrUpdateFile(_b);
-        _testFiles.AddOrUpdateFile(_c);
-    }
-
-    public void Dispose()
-    {
-        GC.SuppressFinalize(this);
-    }
-
     [Fact]
     public void ConstructorSetName()
     {
@@ -39,74 +19,8 @@ public class DirectoryItemTests : IDisposable
         Assert.Throws<ArgumentException>(() => new FileItem("01test"));
         Assert.Throws<ArgumentException>(() => new FileItem("$test"));
         Assert.Throws<ArgumentException>(() => new FileItem("_test"));
-    }
-
-    [Fact]
-    public void GetFile_FileDoesNotExist_ReturnsNull()
-    {
-        var test = new DirectoryItem("test");
-        Assert.Null(test.GetFile("test_file.txt"));
-    }
-
-    [Fact]
-    public void GetFile_FileExists_ReturnsFile()
-    {
-        Assert.NotNull(_testFiles.GetFile("a.txt"));
-    }
-
-    [Fact]
-    public void AddOrUpdateFile_AddsFile()
-    {
-        var test = new FileItem("test.txt");
-        Assert.Null(_testFiles.GetFile("test.txt"));
-
-        _testFiles.AddOrUpdateFile(test);
-        Assert.Equal(test, _testFiles.GetFile("test.txt"));
-    }
-
-    [Fact]
-    public void RemoveFile_Succeeds_ReturnsTrue()
-    {
-        bool result = _testFiles.RemoveFile("a.txt");
-        Assert.Null(_testFiles.GetFile("a.txt"));
-        Assert.True(result);
-    }
-
-    [Fact]
-    public void RemoveFile_Fails_ReturnsFalse()
-    {
-        bool result = _testFiles.RemoveFile("non_existent_file.txt");
-        Assert.False(result);
-    }
-
-    [Fact]
-    public void ContainsFile_FileExists_ReturnsTrue()
-    {
-        bool result = _testFiles.ContainsFile("a.txt");
-        Assert.True(result);
-    }
-
-    [Fact]
-    public void ContainsFile_FileDoesNotExist_ReturnsFalse()
-    {
-        bool result = _testFiles.ContainsFile("non_existent_file.txt");
-        Assert.False(result);
-    }
-
-    [Theory]
-    [InlineData("a.txt")]
-    [InlineData("b.txt")]
-    [InlineData("c.txt")]
-    public void GetAllFiles_ReturnsAllFiles(string name)
-    {
-        var files = _testFiles.GetAllFiles();
-        Assert.Contains(files, f => f.Name == name);
-    }
-
-    [Fact]
-    public void Clear_DeletesAllFiles()
-    {
-        _testFiles.Clear();
-        Assert.Equal(_testFiles.GetAllFiles(), []);
+        Assert.Throws<ArgumentException>(() => new FileItem("_test/"));
+        Assert.Throws<ArgumentException>(() => new FileItem("/_test"));
+        Assert.Throws<ArgumentException>(() => new FileItem("/_test/"));
     }
 }

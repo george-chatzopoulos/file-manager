795629d72a09f104b5e619aa1d17e9e45ffe7f36
george <gchatzopoulos@york.citycollege.eu>
2025-07-27 05:50:06 +0300

    feat(DirectoryItem): Add a custom setter for Path

--- src/FileManager.Models/DirectoryItem.cs
+++ src/FileManager.Models/DirectoryItem.cs
@@ -12,9 +12,10 @@ public sealed class DirectoryItem
     public string Name { get; private set; } = string.Empty;
     public string Path { get; private set; } = "/";
 
-    public DirectoryItem(string name)
+    public DirectoryItem(string name, string path = "/")
     {
         SetName(name);
+        SetPath(path);
     }
 
     public void SetName(string name)
@@ -23,5 +24,13 @@ public sealed class DirectoryItem
             throw new ArgumentException("Invalid directory name", nameof(name));
 
         Name = name;
+    }
+
+    public void SetPath(string path)
+    {
+        if (!Models.IsPathValid(path))
+            throw new ArgumentException($"Invalid path", nameof(path));
+
+        Path = path + Name;
     }
 }

67f4fc18917d28e6741bfea1c881a6392a42d702
george <gchatzopoulos@york.citycollege.eu>
2025-07-27 06:33:20 +0300

    test(DirectoryItem): Add test cases for valid and invalid paths

--- tests/FileManager.Models.Tests/DirectoryItem.cs
+++ tests/FileManager.Models.Tests/DirectoryItem.cs
@@ -9,8 +9,8 @@ public class DirectoryItemTests
     [Fact]
     public void ConstructorSetName()
     {
-        var test = new DirectoryItem("test");
-        Assert.Equal("test", test.Name);
+        var test = new DirectoryItem("test/");
+        Assert.Equal("test/", test.Name);
     }
 
     [Fact]
@@ -22,5 +22,22 @@ public class DirectoryItemTests
         Assert.Throws<ArgumentException>(() => new FileItem("_test/"));
         Assert.Throws<ArgumentException>(() => new FileItem("/_test"));
         Assert.Throws<ArgumentException>(() => new FileItem("/_test/"));
+    }
+
+    [Fact]
+    public void Constructor_SetPath_ThrowsArgumentException()
+    {
+        Assert.Throws<ArgumentException>(() => new DirectoryItem("test/", "incorrect_path/"));
+        Assert.Throws<ArgumentException>(() => new DirectoryItem("test/", "/incorrect_path"));
+    }
+
+    [Theory]
+    [InlineData("/", "/test/")]
+    [InlineData("/home/", "/home/test/")]
+    [InlineData("/home/desktop123/", "/home/desktop123/test/")]
+    public void Path_ParsesCorrectly(string path, string expectedRes)
+    {
+        var file = new DirectoryItem("test/", path);
+        Assert.Equal(expectedRes, file.Path);
     }
 }
